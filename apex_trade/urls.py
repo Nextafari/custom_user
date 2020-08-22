4fef5cec0df958d95d81d16c65159d159ed092d4
Nextafari <chinedue16@gmail.com>
2020-08-22 01:23:34 +0100

    :snake:refactored models

--- apex_trade/urls.py
+++ apex_trade/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apex_api.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls'))
+    # path('rest-auth/', include('rest_auth.urls')),
+    # path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
