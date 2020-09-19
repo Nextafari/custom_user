97f5dba9657659f806620729d8baaf9a5118ea9b
Nextafari <chinedue16@gmail.com>
2020-09-19 13:55:22 +0100

    withdrawal logic

--- wallet/views.py
+++ wallet/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from apex_api.models import UserTransaction
 from rest_framework.response import Response
@@ -7,17 +6,11 @@ from .serializers import UserWalletSerializer
 from drf_yasg.utils import swagger_auto_schema
 from django.core.mail import send_mail
 from django.conf import settings
+from rest_framework.permissions import IsAuthenticated
 
 
-# Create your views here.
-
-"""
-Find a way to connect the wallet to the User Transaction table database
-
-This is more like going to be creating a transaction for the user and sending a notification to admin
-to update recent transactions, this will have permission classes enabled
-"""
 class Deposit(APIView):
+    permission_classes = [IsAuthenticated]
     @swagger_auto_schema(
         request_body=UserWalletSerializer,
         operation_description="Deposit",
@@ -43,16 +36,53 @@ class Deposit(APIView):
                 "Deposit Alert",
                 f'{user} just created a deposit of ${amount} equivalent to BTC{amount_in_btc} change status from pending to approved',
                 settings.EMAIL_HOST_USER,
-                ['chinedue16@gmail.com']
+                ['info@trddex.com']
             )
             return Response(
                 {
                     'data': "Done!! Awaiting approval"
-                }
+                },
+                status=status.HTTP_200_OK
+            )
+
+
+class Withdraw(APIView):
+    permission_classes = [IsAuthenticated]
+    @swagger_auto_schema(
+        request_body=UserWalletSerializer,
+        operation_description="Withdraw",
+        responses={201: 'Created'}
+    )
+    def post(self, request):
+        if request.method == "POST":
+            serializer = UserWalletSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            deposit = UserTransaction.objects.create(
+                user=request.user,
+                status="Pending",
+                transaction_type="Withdraw",
+                amount=serializer.validated_data.get("amount"),
+                payment_method="BTC",
+                amount_in_btc=serializer.validated_data.get("amount_in_btc")
+            )
+            deposit.save()
+            user = request.user
+            amount = serializer.validated_data.get("amount")
+            amount_in_btc = serializer.validated_data.get("amount_in_btc")
+            send_mail(
+                "Deposit Alert",
+                f'{user} just created a Withdrawal of ${amount} equivalent to BTC{amount_in_btc} change status from pending to approved',
+                settings.EMAIL_HOST_USER,
+                ['info@trddex.com']
+            )
+            return Response(
+                {
+                    'data': "Done!! Awaiting approval"
+                },
+                status=status.HTTP_200_OK
             )
 
 """
 Set up a mailgun account for the website, 
 checkout linode for hosting the backend and also the frontend of the web(react)
-Deploy the backend to linode
 """
