db386d803c6ad8227451db24e8ea209f2c28866b
Nextafari <chinedue16@gmail.com>
2020-09-27 16:51:29 +0100

    added whitenoise to middleware

--- apex_trade/settings.py
+++ apex_trade/settings.py
@@ -108,6 +108,7 @@ SWAGGER_SETTINGS = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
