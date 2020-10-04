6d328e96fc6664e593f564f09a694609cc358afc
Nextafari <chinedue16@gmail.com>
2020-10-04 03:13:14 +0100

    reverting to old changes on master

--- apex_trade/settings.py
+++ apex_trade/settings.py
@@ -45,9 +45,6 @@ INSTALLED_APPS = [
     'chart',
     'wallet',
     'rest_framework',
-    # cloudinary
-    'cloudinary_storage',
-    'cloudinary',
     # 'allauth',
     # 'allauth.account',
     'knox',
@@ -218,7 +215,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = Path.joinpath(BASE_DIR, "media")
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = '.storage.WhiteNoiseStaticFilesStorage'
 
 
 # This prints out the email in the console/terminal
@@ -237,14 +234,6 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-
-# Cloudinary Settings
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv("CLOUD_NAME"),
-    'API_KEY': os.getenv("API_KEY"),
-    'API_SECRET': os.getenv("API_SECRET")
-}
 
 
 # Activate Django-Heroku.
