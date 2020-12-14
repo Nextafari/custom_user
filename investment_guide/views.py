6afe64aa1c13208e68a1029905e7a756f469808d
Emmanuel <chinedue16@gmail.com>
2020-12-14 22:05:29 +0100

    :snake: Update:

--- investment_guide/views.py
+++ investment_guide/views.py
@@ -4,7 +4,6 @@ from .models import TradeHistory, Trader
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from drf_yasg.utils import swagger_auto_schema
 
 
 class TraderView(APIView):
